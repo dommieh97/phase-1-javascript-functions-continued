ae170938e246b1032bec46084b0480911867e3c1
Domenique <domogenesis77@gmail.com>
2023-06-12 23:31:10 -0500

    ric flair drip

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = `roller-skate`)
+{
+    return (`This Saturday, I want to ${activity}!`)
+};
+
+function mondayWork(activity = `go to the office`)
+{
+    return (`This Monday, I will ${activity}.`)
+};
+
+function wrapAdjective(activity){
+    return function (flare){
+        return `You are ${activity}${flare}${activity}!`; 
+    }
+
+    
+};
